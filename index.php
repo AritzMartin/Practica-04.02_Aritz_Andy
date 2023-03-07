6fd16f64751758dc45c621b666c4328783819ffb
amartingir <113341165+AritzMartin@users.noreply.github.com>
2023-03-07 13:09:57 +0100

    index

--- index.php
+++ index.php
@@ -5,4 +5,24 @@
 <section class="principal">
   <!-- Aquí irá nuestro formulario -->
 </section>
+<form name="login" accept-charset="utf-8" method="post" action="index.php">
+    <ul>
+      <p>
+        <label for="nombre">Nombre</label>
+        <input type="username" name="nombre" placeholder="Tu nombre." required>
+      </p>
+      <p>
+        <label for="email">Correo electrónico</label>
+        <input type="email" name="email" placeholder="email@servicio.com" required>
+      </p>
+      <p>
+        <label for="mensaje">Mensaje</label>
+        <input type="text" name="mensaje" placeholder="Tu mensaje." required>
+      </p>
+      <p>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </p>
+    </ul>
+  </form>  
+
 
