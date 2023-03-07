b03f247a1bee561fc9a75bf8cc264edcf410b8c5
amartingir <113341165+AritzMartin@users.noreply.github.com>
2023-03-07 13:06:43 +0100

    index

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML>
+<header class="principal">
+   <h1>Mi formulario de contacto HTML5</h1>;
+</header>
+<section class="principal">
+  <!-- Aquí irá nuestro formulario -->
+</section>
+
