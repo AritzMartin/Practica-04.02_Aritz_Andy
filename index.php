9ceb0f9cfeffc4867f14cb178c0e7dd47599c763
amartingir <113341165+AritzMartin@users.noreply.github.com>
2023-03-13 12:00:30 +0100

    index.php

--- index.php
+++ index.php
@@ -4,5 +4,29 @@
 	$db_password="";
 	$db_name="basic_form";
 	$db_table_name="basic_data";
+// Realizamos la conexión con la base de datos
+$db_connection = new mysqli($db_host, $db_user,
+                            $db_password, $db_name);
+$db_connection->set_charset("utf8");
+
+// Comprobamos si la conexión ha sido exitosa
+if ($db_connection){
+
+}else{
+    echo "<p>MySQL no conoce ese usuario y password, y rechaza el intento de conexión</p>";
+ }
+// Validamos que hayan llegado estas variables, y que no esten vacias:
+if (isset($_POST["nombre"], $_POST["email"],
+          $_POST["mensaje"]) and $_POST["nombre"] !="" and
+          $_POST["email"]!="" and $_POST["mensaje"]!="" ){
+
+    // Traspasamos a variables locales, para evitar 
+       complicaciones con las comillas:
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+} else {
+    echo '<p>Por favor, complete el <a href="index.html">formulario</a></p>';
+}
 
 ?>
